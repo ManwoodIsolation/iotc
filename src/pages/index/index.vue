3125bcddf2c17dd1fe41a266c28e3e5de0262cdf
Manwood <3448276249@qq.com>
2025-05-07 21:22:21 +0800

    v1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,61 +1,80 @@
 <template>
   <view>
-      <scroll-view
-          scroll-y
-          class="box"
-      >
-          <view class="item" v-for="item in blueDeviceList" @click="connect(item)">
-              <view>
-                  <text>id: {{ item.deviceId }}</text>    
-              </view>
-              <view>
-                  <text>name: {{ item.name }}</text>  
-              </view>
-          </view>
-      </scroll-view>
-      
-      <button @click="initBlue">初始化蓝牙</button>
-      
-      <button @click="discovery">搜索附近蓝牙设备</button>
-      <button @click="getServices">获取蓝牙服务</button>
-      <button @click="getCharacteristics">获取特征值</button>
-      <button @click="notify">开启消息监听</button>
-      <!-- <button @click="send">发送数据</button> -->
+    <scroll-view scroll-y class="box" v-if="!isConnected">
+      <view class="item" v-for="item in blueDeviceList" @click="connect(item)">
+        <view>
+          <text>id: {{ item.deviceId }}</text>
+        </view>
+        <view>
+          <text>name: {{ item.name }}</text>
+        </view>
+      </view>
+    </scroll-view>
+    <view v-if="isConnected">
+      <text>已连接设备: {{ deviceId }}</text>
+      <!-- <button @click="getServices">获取服务</button> -->
+      <!-- 显示结果和置信度 -->
+      <view v-if="Result && confidence">
+        <text class="result-text">结果: {{ Result }}</text>
+        <text class="confidence-text">置信度: {{ confidence }}</text>
+      </view>
+    </view>
+    <button @click="initBlue" v-if="!isConnected">初始化蓝牙</button>
+
+    <button @click="discovery" v-if="!isConnected">搜索附近蓝牙设备</button>
+    <!-- <button @click="getServices">获取蓝牙服务</button>
+    <button @click="getCharacteristics">获取特征值</button> -->
+    <button @click="notify" v-if="!isListening && isConnected">开启消息监听</button>
+    <button @click="unNotify" v-if="isListening && isConnected">关闭消息监听</button>
+    <!-- <button @click="send">发送数据</button> -->
   </view>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
+// 存储结果和置信度
+const Result = ref('')
+const confidence = ref('')
 // 搜索到的蓝牙设备列表
 const blueDeviceList = ref([])
-
+const isConnected = ref(false)
+const isListening = ref(false)
 // 【1】初始化蓝牙
 function initBlue() {
   uni.openBluetoothAdapter({
-      success(res) {
-          console.log('初始化蓝牙成功')
-          console.log(res)
-      },
-      fail(err) {
-          console.log('初始化蓝牙失败')
-          console.error(err)
-      }
+    success(res) {
+      console.log('初始化蓝牙成功')
+      uni.showToast({
+        title: '初始化蓝牙成功',
+        icon: 'success',
+        duration: 1000
+      })
+      console.log(res)
+    },
+    fail(err) {
+      console.log('初始化蓝牙失败')
+      uni.showToast({
+        title: '初始化蓝牙失败',
+        icon: 'none',
+        duration: 1000
+      })
+      console.error(err)
+    }
   })
 }
 
 // 【2】开始搜寻附近设备
 function discovery() {
   uni.startBluetoothDevicesDiscovery({
-      success(res) {
-          console.log('开始搜索')
-          // 开启监听回调
-          uni.onBluetoothDeviceFound(found)
-      },
-      fail(err) {
-          console.log('搜索失败')
-          console.error(err)
-      }
+    success(res) {
+      console.log('开始搜索')
+      // 开启监听回调
+      uni.onBluetoothDeviceFound(found)
+    },
+    fail(err) {
+      console.log('搜索失败')
+      console.error(err)
+    }
   })
 }
 
@@ -75,93 +94,111 @@ function connect(data) {
   deviceId.value = data.deviceId
 
   uni.createBLEConnection({
-      deviceId: deviceId.value,
-      success(res) {
-          console.log('连接成功')
-          console.log(res)
-          // 停止搜索
-          stopDiscovery()
-      },
-      fail(err) {
-          console.log('连接失败')
-          console.error(err)
-      }
+    deviceId: deviceId.value,
+    success(res) {
+      console.log('连接成功')
+      console.log(res)
+      // 停止搜索
+      isConnected.value = true
+      stopDiscovery()
+    },
+    fail(err) {
+      console.log('连接失败')
+      console.error(err)
+    }
   })
 }
 
 // 【5】停止搜索
 function stopDiscovery() {
   uni.stopBluetoothDevicesDiscovery({
-      success(res) {
-          console.log('停止成功')
-          console.log(res)
-      },
-      fail(err) {
-          console.log('停止失败')
-          console.error(err)
-      }
+    success(res) {
+      console.log('停止成功')
+      console.log(res)
+    },
+    fail(err) {
+      console.log('停止失败')
+      console.error(err)
+    }
   })
 }
 // 【6】获取服务
 function getServices() {
-    uni.getBLEDeviceServices({
-        deviceId: deviceId.value, // 设备ID，在上一步【4】里获取
-        success(res) {
-            console.log(res)
-        },
-        fail(err) {
-            console.error(err)
-        }
-    })
+  uni.getBLEDeviceServices({
+    deviceId: deviceId.value, // 设备ID，在上一步【4】里获取
+    success(res) {
+      console.log(res)
+    },
+    fail(err) {
+      console.error(err)
+    }
+  })
 }
 // 【7】获取特征值
 function getCharacteristics() {
-    uni.getBLEDeviceCharacteristics({
-        deviceId: deviceId.value, // 设备ID，在【4】里获取到
-        serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
-        success(res) {
-            console.log(res)
-        },
-        fail(err) {
-            console.error(err)
-        }
-    })
+  uni.getBLEDeviceCharacteristics({
+    deviceId: deviceId.value, // 设备ID，在【4】里获取到
+    serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
+    success(res) {
+      console.log(res)
+    },
+    fail(err) {
+      console.error(err)
+    }
+  })
 }
 // 【9】监听消息变化
 function listenValueChange() {
-  console.log('监听消息变化');
-  
-    uni.onBLECharacteristicValueChange(res => {
-        // 结果
-        console.log('监听消息变化',res)
-        
-        // 结果里有个value值，该值为 ArrayBuffer 类型，所以在控制台无法用肉眼观察到，必须将该值转换为16进制
-        let resHex = ab2hex(res.value)
-        // console.log(resHex)
+  // console.log('监听消息变化');
+  uni.showToast({
+    title: '监听消息变化已开启',
+    icon: 'none',
+    duration: 1000
+  })
+  uni.onBLECharacteristicValueChange(res => {
+    // 结果
+    console.log('监听消息变化', res)
 
-        // 最后将16进制转换为ascii码，就能看到对应的结果
-        let result = hexCharCodeToStr(resHex)
-        console.log(result)
-    })
+    // 结果里有个value值，该值为 ArrayBuffer 类型，所以在控制台无法用肉眼观察到，必须将该值转换为16进制
+    let resHex = ab2hex(res.value)
+    // console.log(resHex)
+
+    // 最后将16进制转换为ascii码，就能看到对应的结果
+    let result = hexCharCodeToStr(resHex)
+    console.log(result)
+    // 显示结果和置信度
+    // 使用正则表达式提取result和confidence的值
+    const resultMatch = result.match(/Result :\s*(.*?)\s*confidence \s*(\d+\.\d+)/)
+    if (resultMatch) {
+      Result.value = resultMatch[1]
+      confidence.value = parseFloat(resultMatch[2])
+      console.log('提取的结果:', Result.value)
+      console.log('提取的置信度:', confidence.value)
+    } else {
+      console.log('无法提取结果和置信度')
+    }
+  })
 
 }
 
 // 【8】开启消息监听
 function notify() {
-    uni.notifyBLECharacteristicValueChange({
-        deviceId: deviceId.value, // 设备ID，在【4】里获取到
-        serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
-        characteristicId: '0000FFE1-0000-1000-8000-00805F9B34FB', // 特征值，在【7】里能获取到
-        state: true,
-        success(res) {
-            console.log("开启消息监听成功", res)
-            // 接受消息的方法
-            listenValueChange()
-        },
-        fail(err) {
-            console.error("开启消息监听error",err)
-        }
-    })
+  getServices()
+  getCharacteristics()
+  uni.notifyBLECharacteristicValueChange({
+    deviceId: deviceId.value, // 设备ID，在【4】里获取到
+    serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
+    characteristicId: '0000FFE1-0000-1000-8000-00805F9B34FB', // 特征值，在【7】里能获取到
+    state: true,
+    success(res) {
+      console.log("开启消息监听成功", res)
+      // 接受消息的方法
+      listenValueChange()
+    },
+    fail(err) {
+      console.error("开启消息监听error", err)
+    }
+  })
 }
 
 // ArrayBuffer转16进度字符串示例
@@ -177,47 +214,47 @@ function ab2hex(buffer) {
 
 // 将16进制的内容转成我们看得懂的字符串内容
 function hexCharCodeToStr(hexCharCodeStr) {
-    var trimedStr = hexCharCodeStr.trim();
-    var rawStr = trimedStr.substr(0, 2).toLowerCase() === "0x" ? trimedStr.substr(2) : trimedStr;
-    var len = rawStr.length;
-    if (len % 2 !== 0) {
-            alert("存在非法字符!");
-            return "";
-    }
-    var curCharCode;
-    var resultStr = [];
-    for (var i = 0; i < len; i = i + 2) {
-            curCharCode = parseInt(rawStr.substr(i, 2), 16);
-            resultStr.push(String.fromCharCode(curCharCode));
-    }
-    return resultStr.join("");
+  var trimedStr = hexCharCodeStr.trim();
+  var rawStr = trimedStr.substr(0, 2).toLowerCase() === "0x" ? trimedStr.substr(2) : trimedStr;
+  var len = rawStr.length;
+  if (len % 2 !== 0) {
+    alert("存在非法字符!");
+    return "";
+  }
+  var curCharCode;
+  var resultStr = [];
+  for (var i = 0; i < len; i = i + 2) {
+    curCharCode = parseInt(rawStr.substr(i, 2), 16);
+    resultStr.push(String.fromCharCode(curCharCode));
+  }
+  return resultStr.join("");
 }
 // 【10】发送数据
 function send() {
-    // 向蓝牙设备发送一个0x00的16进制数据
-    
-    let msg = 'hello'
-    
-    const buffer = new ArrayBuffer(msg.length)
-    const dataView = new DataView(buffer)
-    // dataView.setUint8(0, 0)
-    
-    for (var i = 0; i < msg.length; i++) {
-      dataView.setUint8(i, msg.charAt(i).charCodeAt())
+  // 向蓝牙设备发送一个0x00的16进制数据
+
+  let msg = 'hello'
+
+  const buffer = new ArrayBuffer(msg.length)
+  const dataView = new DataView(buffer)
+  // dataView.setUint8(0, 0)
+
+  for (var i = 0; i < msg.length; i++) {
+    dataView.setUint8(i, msg.charAt(i).charCodeAt())
+  }
+
+  uni.writeBLECharacteristicValue({
+    deviceId: deviceId.value, // 设备ID，在【4】里获取到
+    serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
+    characteristicId: '0000FFE1-0000-1000-8000-00805F9B34FB', // 特征值，在【7】里能获取到
+    value: buffer,
+    success(res) {
+      console.log("发送数据成功", res)
+    },
+    fail(err) {
+      console.error("发送数据失败", err)
     }
-    
-    uni.writeBLECharacteristicValue({
-      deviceId: deviceId.value, // 设备ID，在【4】里获取到
-      serviceId: '0000FFE0-0000-1000-8000-00805F9B34FB', // 服务UUID，在【6】里能获取到
-      characteristicId: '0000FFE1-0000-1000-8000-00805F9B34FB', // 特征值，在【7】里能获取到
-      value: buffer,
-      success(res) {
-        console.log("发送数据成功",res)
-      },
-      fail(err) {
-        console.error("发送数据失败",err)
-      }
-    })
+  })
 }
 
 </script>
@@ -230,11 +267,13 @@ function send() {
   margin-bottom: 20rpx;
   border: 2px solid dodgerblue;
 }
+
 .item {
   box-sizing: border-box;
   padding: 10rpx;
   border-bottom: 1px solid #ccc;
 }
+
 button {
   margin-bottom: 20rpx;
 }
